81f19cd53e94bac16435b16473c6965017e52959
Rafik Margaryan <861186+raffdoc@users.noreply.github.com>
2025-02-21 08:12:32 +0000

    code is added fro tabular data

--- source-code/tabular/tabulate.R
+++ source-code/tabular/tabulate.R
@@ -1,2 +1,61 @@
-votes <- read.table("votes.csv")
+# basics file reading
+votes <- read.table("data/votes.csv")
 View(votes)
+# some other options
+votes <- read.table(
+  "votes.csv",
+  sep = ","
+)
+View(votes)
+# adding header 
+votes <- read.table(
+  "votes.csv",
+  sep = ",",
+  header = TRUE
+)
+View(votes)
+# dedicated funcion
+votes <- read.csv("data/votes.csv")
+View(votes)
+
+# subsetting the valus 
+
+votes[, 1]
+votes[, 2]
+votes[, 3]
+
+# subseting with names
+
+colnames(votes)
+
+votes$candidate
+votes$poll
+votes$mail
+
+# sum the votes
+sum(votes$poll[1], votes$poll[2], votes$poll[3])
+
+# sum all the values in one variable
+sum(votes$poll)
+sum(votes$mail)
+
+# pairwise suming
+
+votes$poll[1] + votes$mail[1]
+votes$poll[2] + votes$mail[2]
+votes$poll[3] + votes$mail[3]
+# pairvise all summation
+
+votes$poll + votes$mail
+
+# summa variable into new variable
+votes$total <- votes$poll + votes$mail
+
+votes$total <- votes$poll + votes$mail
+
+write.csv(votes, "data/totals.csv")
+
+# avoid raw names
+votes$total <- votes$poll + votes$mail
+
+write.csv(votes, "totals.csv", row.names = FALSE)
