bd560640fa0eaaa5528ca152ec86425b3c2d6144
Rafik Margaryan <861186+raffdoc@users.noreply.github.com>
2025-02-21 12:01:49 +0000

    finetuning sales file.

--- source-code/sales/sales.R
+++ source-code/sales/sales.R
@@ -33,19 +33,4 @@ View(sales)
 
 # Demonstrates flagging sales as high value
 
-Q1 <- read.csv("data/Q1.csv")
-Q1$quarter <- "Q1"
-
-Q2 <- read.csv("data/Q2.csv")
-Q2$quarter <- "Q2"
-
-Q3 <- read.csv("data/Q3.csv")
-Q3$quarter <- "Q3"
-
-Q4 <- read.csv("data/Q4.csv")
-Q4$quarter <- "Q4"
-
-sales <- rbind(Q1, Q2, Q3, Q4)
-View(sales)
-
 sales$value <- ifelse(sales$sale_amount > 100, "High Value", "Regular")
