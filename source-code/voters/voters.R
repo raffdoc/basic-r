4f31f5269be7bec582ab69a53e7fc64c829418f6
Rafik Margaryan <861186+raffdoc@users.noreply.github.com>
2025-02-21 10:07:13 +0000

    voters file added to demostrate the remote data loading

--- source-code/voters/voters.R
+++ source-code/voters/voters.R
@@ -3,28 +3,17 @@ url <- "https://github.com/fivethirtyeight/data/raw/master/non-voters/nonvoters_
 voters <- read.csv(url)
 # Demonstrates finding number of rows and columns in a large data set
 
-url <- "https://github.com/fivethirtyeight/data/raw/master/non-voters/nonvoters_data.csv"
-voters <- read.csv(url)
-
 nrow(voters)
 ncol(voters)
 # Demonstrates finding unique values in a vector
 
-url <- "https://github.com/fivethirtyeight/data/raw/master/non-voters/nonvoters_data.csv"
-voters <- read.csv(url)
 
 unique(voters$voter_category)
 # Demonstrates NA
 
-url <- "https://github.com/fivethirtyeight/data/raw/master/non-voters/nonvoters_data.csv"
-voters <- read.csv(url)
-
 voters$Q22
 unique(voters$Q22)
 # Demonstrates converting a vector to a factor
-
-url <- "https://github.com/fivethirtyeight/data/raw/master/non-voters/nonvoters_data.csv"
-voters <- read.csv(url)
 
 voters$Q21
 
@@ -38,11 +27,9 @@ factor(
 )
 # Demonstrates excluding values from the levels of a factor
 
-url <- "https://github.com/fivethirtyeight/data/raw/master/non-voters/nonvoters_data.csv"
-voters <- read.csv(url)
-
 voters$Q21 <- factor(
   voters$Q21,
   labels = c("Yes", "No", "Unsure/Undecided"),
   exclude = c(-1)
 )
+levels(voters$Q21)
