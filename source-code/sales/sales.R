95aea756be3de600e26e2f43c2f9a65cbabffff1
Rafik Margaryan <861186+raffdoc@users.noreply.github.com>
2025-02-21 11:57:40 +0000

    edit sales

--- source-code/sales/sales.R
+++ source-code/sales/sales.R
@@ -7,10 +7,10 @@ Q4 <- read.csv("data/Q4.csv")
 
 # Combines data frames with `rbind`
 
-Q1 <- read.csv("Q1.csv")
-Q2 <- read.csv("Q2.csv")
-Q3 <- read.csv("Q3.csv")
-Q4 <- read.csv("Q4.csv")
+Q1 <- read.csv("data/Q1.csv")
+Q2 <- read.csv("data/Q2.csv")
+Q3 <- read.csv("data/Q3.csv")
+Q4 <- read.csv("data/Q4.csv")
 
 sales <- rbind(Q1, Q2, Q3, Q4)
 # Adds quarter column to data frames
