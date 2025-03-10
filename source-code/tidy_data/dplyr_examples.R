4761d5ed950c8612c3876926e1ede2ecb2cc44b4
raffdoc <raffdoc@gmail.com>
2025-03-10 13:10:19 +0100

    update on dplyr

--- source-code/tidy_data/dplyr_examples.R
+++ source-code/tidy_data/dplyr_examples.R
@@ -18,3 +18,13 @@ students |>
 
 library(readr)
 health <- read_csv("data/healthcare_dataset.csv")
+health
+
+health |> 
+  select(Age, Gender, `Blood Type`) |>
+  filter(`Blood Type` %in% c("O+", "O-")) |>
+  group_by(Gender, `Blood Type`) |>
+  summarise(mean_age = mean(Age), 
+            sd_age = sd(Age), 
+            n_age = n()
+            )
---
+++ source-code/vis_data/ggplot2.R
@@ -0,0 +1,3 @@
+library(readr)
+health <- read_csv("data/healthcare_dataset.csv")
+health
