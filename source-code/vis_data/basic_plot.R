cbc6a2d9a8b39c71961904e5cf84e0e465a434c2
raffdoc <raffdoc@gmail.com>
2025-03-10 14:17:11 +0100

    dplyr and ggplot example added.

---
+++ source-code/vis_data/basic_plot.R
@@ -0,0 +1,1 @@
+# basic ploting system
--- source-code/vis_data/ggplot2.R
+++ source-code/vis_data/ggplot2.R
@@ -1,3 +1,48 @@
 library(readr)
+library(ggplot2)
+library(Hmisc)
 health <- read_csv("data/healthcare_dataset.csv")
 health
+
+
+# visualizing age histogram
+health |> 
+  select(Age, Gender) |>
+  group_by(Gender)|>
+  ggplot(aes(x = Age)) + 
+  geom_histogram() +
+  facet_wrap(~Gender) +
+  geom_vline(aes(xintercept = mean(Age)), linetype = 'dotted', colour = 'red')
+
+# boxplot example
+
+health |> 
+  select(Age, Gender, `Medical Condition`) |>
+  group_by(Gender)|>
+  ggplot(aes(x = Gender,y = Age)) + 
+  geom_boxplot() +
+  facet_wrap(~`Medical Condition`) #+
+#  geom_vline(aes(xintercept = mean(Age)), linetype = 'dotted', colour = 'red')
+
+# ggplot thems and colors
+
+health |> 
+  select(Age, Gender, `Medical Condition`) |>
+  group_by(Gender)|>
+  ggplot(aes(x = Gender,y = Age)) + 
+  geom_boxplot() +
+  facet_wrap(~`Medical Condition`) +
+  theme_minimal() +
+  xlab("Sex of the patients") +
+  ylab("Distribution of the patient")
+
+# advanced libraries 
+library(ggstatsplot)
+set.seed(123)
+
+ggbetweenstats(
+  data  = health,
+  x     = Gender,
+  y     = Age,
+  title = "Distribution of sepal length across Iris species"
+) + theme_bw()
